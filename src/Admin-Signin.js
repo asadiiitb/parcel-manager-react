c012bf29eb7fafe82831ad97b16075ee4214247c
Mohammed Asad Ansari <mohammadasad.ansari@iiitb.org>
2021-05-15 03:17:19 +0530

    Added Services.jsx and converted signin.js function to class

--- src/Admin-Signin.js
+++ src/Admin-Signin.js
@@ -1,11 +1,53 @@
-import {React,Fragment} from 'react'
+import {React,Fragment, Component} from 'react'
+import axios from "axios";
 import sign_up from "./images/signup-image.jpg"
 import sign_in from "./images/signin-image.jpg"
 import Signupadmin from "./Sign-up-Admin"
+
 import "./css/style.css"
 
-function Admin_Signin() {
-  return (
+class Admin_Signin extends Component {
+    
+        constructor(props) {
+        super(props);
+        this.state = {
+            emailId: "",
+            password: "",
+            errorMessage : false
+        }
+        this.EmailHandler=this.EmailHandler.bind(this)
+        this.PasswordHandler=this.PasswordHandler.bind(this)
+        this.handleClick= this.handleClick.bind(this)
+    }
+
+    EmailHandler= (event) => {
+        this.setState({emailId: event.target.value});
+    }
+    PasswordHandler= (event) => {
+        this.setState({password: event.target.value});
+    }
+    handleClick= (event) =>{
+        event.preventDefault();
+        let customer = {
+            emailId: this.state.emailId,
+            password: this.state.password
+        }
+        axios.post('http://localhost:8082/api/customer/login',customer)
+            .then(response =>{
+                console.log(response);
+                console.log(response.data);
+                {this.props.history.push('/');
+                    console.log("login");
+                }
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+        alert('login');
+    }
+
+    render(){
+      return (
     		<Fragment>
 
     		<section class="sign-in">
@@ -21,18 +63,18 @@ function Admin_Signin() {
                         <form method="POST" class="register-form" id="login-form">
                             <div class="form-group">
                                 <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="your_name" id="your_name" placeholder="Your Name"/>
+                                <input type="text" name="your_name" id="your_name" placeholder="Your Name"    value={this.state.emailId} onChange={this.EmailHandler}/>
                             </div>
                             <div class="form-group">
                                 <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="your_pass" id="your_pass" placeholder="Password"/>
+                                <input type="password" name="your_pass" id="your_pass" placeholder="Password" value={this.state.password} onChange={this.PasswordHandler}/>
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
                                 <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
                             </div>
                             <div class="form-group form-button">
-                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
+                                <input type="submit" name="signin" id="signin" class="form-submit" onClick={this.handleClick} value="Log in"/>
                             {/* IF login success ful go to Admin Options else show alert */}
                             </div>
                         </form>
@@ -47,10 +89,12 @@ function Admin_Signin() {
                     </div>
                 </div>
             </div>
-        </section>       
+            </section>       
        
     		</Fragment>
     	)
+  }
+
 }
 
 export default Admin_Signin;
--- src/Sign-up-Admin.js
+++ src/Sign-up-Admin.js
@@ -1,9 +1,10 @@
-import {React,Fragment} from 'react'
+import {React,Fragment, Component} from 'react'
 import sign_up from "./images/signup-image.jpg"
 import sign_in from "./images/signin-image.jpg"
 import "./css/style.css"
 
-function Signupadmin() {
+class Signupadmin extends Component{
+  render(){
   return (
     		<Fragment>
 	
@@ -63,6 +64,7 @@ function Signupadmin() {
        
     		</Fragment>
     	)
+    }
 }
 
 export default Signupadmin;
